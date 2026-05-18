a3ce701ba7ade2e86280a1ec22e4183e61cf3ae9
HuynhNghiax <146554474+HuynhNghiax@users.noreply.github.com>
2026-05-18 22:54:53 +0700

    resolve edge-to-edge crash and finalize login UI skeleton

---
+++ frontend-android/app/src/main/java/com/example/smartnotesfrontend/ui/auth/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartnotesfrontend.ui.auth;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.smartnotesfrontend.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Nạp file giao diện XML của bạn vào Activity
+        setContentView(R.layout.activity_login);
+
+        // Code xử lý sự kiện nút bấm, lấy dữ liệu Email/Password sẽ viết tiếp ở đây
+    }
+}
